c5bd1534806a5bb1e7ac83f0ccfbcf41133ed4e9
Hildred N. Baker <nikkimnik@gmail.com>
2022-10-09 20:23:14 -0400

    first major updates

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 # hildrednbaker
+<script>
+"use strict";
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+</script>
